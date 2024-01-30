fb97b8ec071329d51a507d17feead0f7d4177f1c
Irina Vinogorskaya <bigshprot@yahoo.com>
2024-01-30 17:25:51 +0300

    добавляет форму

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__button');
+
+nav.classList.add('nav--closed');
+navButton.addEventListener('click', () => {
+nav.classList.toggle('nav--closed');
+nav.classList.toggle('nav--opened');
+});
